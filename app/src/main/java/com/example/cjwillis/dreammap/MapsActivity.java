71223c738aef7733e1d2bf58950d118dcf014c1a
CJ Willis <cjwillis809@gmail.com>
2015-09-29 15:47:00 -0500

    Top 5 Cities to travel markers
    Added 5 markers to the map of places I want to go. Used a for loop of a
    keySet in a hash map to populate the markers.

--- app/src/main/java/com/example/cjwillis/dreammap/MapsActivity.java
+++ app/src/main/java/com/example/cjwillis/dreammap/MapsActivity.java
@@ -3,20 +3,35 @@ package com.example.cjwillis.dreammap;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.example.cjwillis.dreammap.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.HashMap;
+
 public class MapsActivity extends FragmentActivity {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleMap map; // Might be null if Google Play services APK is not available.
+    private HashMap<String, LatLng> locations = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        setUpLocations();
         setUpMapIfNeeded();
+    }
+
+    private void setUpLocations() {
+        locations.put("Home", new LatLng(41.882054, -87.627815));
+        locations.put("Tokyo, Japan", new LatLng(35.6895000	, 139.6917100));
+        locations.put("Toronto, Canada", new LatLng(43.652449, -79.379431));
+        locations.put("Paris, France", new LatLng(48.858398, 2.294703));
+        locations.put("New York City", new LatLng(40.759102, -73.985131));
+        locations.put("London, United Kingdom", new LatLng(51.500713, -0.124629));
     }
 
     @Override
@@ -28,7 +43,7 @@ public class MapsActivity extends FragmentActivity {
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * call {@link #setUpMap()} once when {@link #map} is not null.
      * <p/>
      * If it isn't installed {@link SupportMapFragment} (and
      * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
@@ -42,12 +57,12 @@ public class MapsActivity extends FragmentActivity {
      */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
+        if (map == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+            map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
             // Check if we were successful in obtaining the map.
-            if (mMap != null) {
+            if (map != null) {
                 setUpMap();
             }
         }
@@ -57,9 +72,13 @@ public class MapsActivity extends FragmentActivity {
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
      * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     * This should only be called once and when we are sure that {@link #map} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        for (String key : locations.keySet()){
+            map.addMarker(new MarkerOptions().position(locations.get(key)).title(key));
+        }
+//        map.addMarker(new MarkerOptions().position(new LatLng(41.882054, -87.627815)).title("Home"));
+
     }
 }
